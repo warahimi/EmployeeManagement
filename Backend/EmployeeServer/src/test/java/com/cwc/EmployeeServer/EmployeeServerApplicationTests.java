73814d63f92c35798bf3a33955c94f6781505163
Wahidullah Rahimi <wahidrahimi45@gmail>
2025-06-27 08:56:58 -0700

    first commit

---
+++ Backend/EmployeeServer/src/test/java/com/cwc/EmployeeServer/EmployeeServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cwc.EmployeeServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
